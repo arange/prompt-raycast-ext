36c367b5a40c48966859a14cf6160736b1de8986
Pan Zhan <pzhan@officeworks.com.au>
2023-03-26 10:09:33 +1100

    refactor(lint): prettier lint

--- src/constants/prompts.ts
+++ src/constants/prompts.ts
@@ -1,3 +1,8 @@
-const PROFESSIONAL_REPHRASE = { description: "Help you sound more professional in bussiness settings", system: "You are an assistant who is familiar with workplace language, and you can help me make my expression more professional", user: "" }
+const PROFESSIONAL_REPHRASE = {
+  description: "Help you sound more professional in bussiness settings",
+  system:
+    "You are an assistant who is familiar with workplace language, and you can help me make my expression more professional",
+  user: "",
+};
 
-export { PROFESSIONAL_REPHRASE }
+export { PROFESSIONAL_REPHRASE };
--- src/hooks/useAPIKEY.ts
+++ src/hooks/useAPIKEY.ts
@@ -2,16 +2,16 @@ import { getPreferenceValues } from "@raycast/api";
 import { useState } from "react";
 
 interface Preferences {
-    API_KEY: string;
+  API_KEY: string;
 }
 
 export const useAPIKEY = () => {
-    const [API_KEY] = useState(() => getApiKey());
+  const [API_KEY] = useState(() => getApiKey());
 
-    function getApiKey() {
-        const preferences = getPreferenceValues<Preferences>();
-        return preferences.API_KEY;
-    }
+  function getApiKey() {
+    const preferences = getPreferenceValues<Preferences>();
+    return preferences.API_KEY;
+  }
 
-    return { key: API_KEY }
-}
+  return { key: API_KEY };
+};
